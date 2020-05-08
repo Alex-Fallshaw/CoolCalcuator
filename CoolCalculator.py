b4af9baa89918cc6620c741a74f6a654916254ee
Alex Fallshaw <alex@fallshaw.me>
2020-05-08 18:05:01 +1000

    Quadratics and Discriminant
    Now user inputs a quadratic and out put is its discriminant

--- CoolCalculator.py
+++ CoolCalculator.py
@@ -1,32 +1,35 @@
 import PySimpleGUI as sg
 import re
 
-sg.theme('DarkAmber')	# Add a touch of colour
+sg.theme('DarkTeal4')	# Add a touch of colour
 
 re_numbers = re.compile(r"^\d+(\.\d+)?$")
 
 # Strings
-exit_button_text = 'Leave'
+exit_button_text = 'Leave' 
 calculate_button_text = 'Calculate'
 
 # All the stuff inside your window.
-layout = [  [sg.Text('Input first number:'), sg.InputText()],
-            [sg.Text('Input second number:'), sg.InputText()],
-            [sg.Text('Input third number:'), sg.InputText()],
-            [sg.Text('This is where your sum will show.', size=(100,1), key="sum")],
+layout = [  [sg.Text('Quadratic-o-matic')],
+            [sg.Text('Input your quadratic:'), sg.InputText(size=(5,1), key='paramA'),
+             sg.Text('x^2 +'), sg.InputText(size=(5,1), key='paramB'),
+             sg.Text('x +'), sg.InputText(size=(5,1), key='paramC')],
+            [sg.Text('This is where the discriminant will show.', size=(100,1), key="sumText")],
             [sg.Button(calculate_button_text), sg.Button(exit_button_text)] ]
 
 # Create the Window
 window = sg.Window('Number summer', layout)
 # Event Loop to process "events" and get the "values" of the inputs
+
 while True:
     event, values = window.read()
     if event in (None, exit_button_text):	# if user closes window or clicks cancel
         break
-    if re.match(re_numbers, values[0]) and re.match(re_numbers, values[1]) and re.match(re_numbers, values[2]):
-        sum=float(values[0])+float(values[1])+float(values[2])
-        window["sum"].update('The sum of your numbers is ' + str(round(sum, 5)))
+    if re.match(re_numbers, values['paramA']) and re.match(re_numbers, values['paramB']) and re.match(re_numbers, values['paramC']):
+        params={'a': float(values['paramA']), 'b': float(values['paramB']), 'c': float(values['paramC'])}
+        discrim=params['a']**2.0 - 4.0*params['b']*params['c']
+        window["sumText"].update('The discriminant is ' + str(discrim))
     else:
-        window["sum"].update('Give me real numbers, dickhead.')
+        window["sumText"].update('Give me real numbers, stupid.')
 
 window.close()
